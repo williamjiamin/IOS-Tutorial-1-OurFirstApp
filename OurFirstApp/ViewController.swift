46a8b311be169b16144ab23172ee9084fd1c8764
williamjiamin <lipai_wang@163.com>
2022-10-08 11:30:11 +0800

    Initial Commit

---
+++ OurFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OurFirstApp
+//
+//  Created by william on 2022/10/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
